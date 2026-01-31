39857170db33add6dc41f391e7107e5b63aa6ff1
Mihaela Diaconu <contact.mihaela.office@gmail.com>
2026-01-31 19:12:39 +0200

    docker files

--- userservice/src/main.ts
+++ userservice/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        url: '0.0.0.0:50051',   // 👈 AICI
+        package: 'user',
+        protoPath: join(__dirname, '../proto/user.proto'),
+      },
+    },
+  );
+
+  await app.listen();
 }
 bootstrap();
