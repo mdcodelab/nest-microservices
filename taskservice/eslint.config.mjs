d5916cb812faf6175e2d208010ca0a4c593f2041
Mihaela Diaconu <contact.mihaela.office@gmail.com>
2026-01-31 02:31:48 +0200

    taskservice

---
+++ taskservice/eslint.config.mjs
@@ -0,0 +1,41 @@
+/ @ts-checimport eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'module', // recomandat pentru Nest
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      // dezactivează erorile TS care urlă pe Nest
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
+
+      // Prettier
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
+  },
+);
+
+
---
+++ taskservice/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module, OnModuleInit } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule, InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+//import { UsersModule } from './users/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
+    //UsersModule
+  ],
+})
+export class AppModule implements OnModuleInit {
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+
+  onModuleInit() {
+    if (this.connection.readyState === 1) {
+      console.log('✅ MongoDB connected!');
+    } else {
+      console.log('❌ MongoDB not connected, readyState:', this.connection.readyState);
+    }
+  }
+}
