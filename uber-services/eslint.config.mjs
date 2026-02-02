2b90a31497c47298552b8dca82bfdd101580aab1
Mihaela Diaconu <contact.mihaela.office@gmail.com>
2026-02-02 11:44:02 +0200

    restructure app - ober-services

--- uber-services/eslint.config.mjs
+++ uber-services/eslint.config.mjs
@@ -1,4 +1,5 @@
-/ @ts-checimport eslint from '@eslint/js';
+// @ts-check
+import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -16,7 +17,7 @@ export default tseslint.config(
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'module', // recomandat pentru Nest
+      sourceType: 'commonjs',
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
@@ -25,17 +26,10 @@ export default tseslint.config(
   },
   {
     rules: {
-      // dezactivează erorile TS care urlă pe Nest
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'off',
-      '@typescript-eslint/no-floating-promises': 'off',
-
-      // Prettier
-      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      "prettier/prettier": ["error", { endOfLine: "auto" }],
     },
   },
 );
-
-
